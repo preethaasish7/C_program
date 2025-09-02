0ae0747f6de0da3019ad8e581bc937bdc2c66b65
preethaasish7 <preethaasish7@gmail.com>
2025-09-02 11:22:37 +0530

    Add files via upload

---
+++ CONTINUE.C
@@ -0,0 +1,15 @@
+main()
+{
+int i;
+clrscr();
+for(i=1;i<=10;i++)
+{
+printf("%d\n",i);
+if(i==8)
+{
+break;
+}
+}
+getch();
+return 0;
+}
---
+++ DO_WHILE.C
@@ -0,0 +1,12 @@
+main()
+{
+int i=100;
+clrscr();
+do
+{
+printf("%d\n",i);
+i=i-2;
+}while(i>=2);
+getch();
+return 0;
+}
